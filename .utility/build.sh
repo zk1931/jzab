d8ca456a7bc67538ada7a1fe071cbf3a84308c08
m1ch1 <michim@apache.org>
2014-07-10 22:36:28 -0700

    travis: print env before build for debugging.

--- .utility/build.sh
+++ .utility/build.sh
@@ -3,8 +3,10 @@
 if [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] &&
    [ "$TRAVIS_PULL_REQUEST" == "false" ] &&
    [ "$TRAVIS_BRANCH" == "master" ]; then
+  env
   mvn clean deploy
 else
+  env
   mvn clean verify
 fi
 
