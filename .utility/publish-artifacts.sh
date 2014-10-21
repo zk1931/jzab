827c6f984917e5879235668f547706afd7fcd0f0
m1ch1 <michim@apache.org>
2014-10-20 23:35:57 -0700

    Add a documentation directory.

--- .utility/publish-artifacts.sh
+++ .utility/publish-artifacts.sh
@@ -6,21 +6,22 @@ if [ "$TRAVIS_REPO_SLUG" == "zk1931/jzab" ] &&
 
   # Push javadoc.
   # http://benlimmer.com/2013/12/26/automatically-publish-javadoc-to-gh-pages-with-travis-ci/
-  echo "Publishing javadoc..."
+  echo "Publishing documentation"
 
   cp -R target/site/apidocs $HOME/javadoc-latest
+  cp -R doc $HOME/doc-latest
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/zk1931/jzab gh-pages > /dev/null
-
   cd gh-pages
+  git rm -rf --ignore-unmatch $TRAVIS_BRANCH
   mkdir -p $TRAVIS_BRANCH
-  git rm -rf --ignore-unmatch $TRAVIS_BRANCH/javadoc
+  cp -Rf $HOME/doc-latest $TRAVIS_BRANCH
   cp -Rf $HOME/javadoc-latest $TRAVIS_BRANCH/javadoc
   git add -f .
-  git commit -m "Push javadoc from travis build $TRAVIS_BUILD_NUMBER to gh-pages."
+  git commit -m "Push documentation from travis build $TRAVIS_BUILD_NUMBER to gh-pages."
   git push -fq origin gh-pages > /dev/null
 
-  echo "Published javadoc to gh-pages."
+  echo "Published documentation to gh-pages."
 fi
